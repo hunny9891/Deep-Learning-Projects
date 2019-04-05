563cbcc30477c3048fb3ea88f555912635d30b27
hunny9891 <hunny9891@gmail.com>
2019-04-05 10:27:01 +0530

    Reduce batch size to 64

--- CIFAR10 Image Classification/main.py
+++ CIFAR10 Image Classification/main.py
@@ -59,7 +59,7 @@ def main():
     print('Shape of test labels: ' + str(y_test.shape))
     
     model = CustomModel()
-    trained_model = model.train_with_custom_resnet50(X_train, y_train, X_test, y_test, num_epochs, 128)
+    trained_model = model.train_with_custom_resnet50(X_train, y_train, X_test, y_test, num_epochs, 64)
 
 if __name__ == "__main__":
     main()
