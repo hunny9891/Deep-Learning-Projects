a709fc0c2ff2028f7d13cd6de3510267ce589f08
hunny9891 <hunny9891@gmail.com>
2019-04-05 13:14:39 +0530

    Cifar 100 model fix

--- CIFAR100 Image Classification/code/resnet50.py
+++ CIFAR100 Image Classification/code/resnet50.py
@@ -1,13 +1,15 @@
 import numpy as np
-import tensorflow as tf
 
-from tensorflow import keras
 from keras.models import Model
 from keras.layers import Dense, AveragePooling2D, MaxPooling2D, Activation,ZeroPadding2D, Flatten, Input, Dropout, Conv2D, BatchNormalization, Add
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 from keras.initializers import glorot_uniform
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+
+from util import Utility
 
 class CustomModel:
     def __init__(self):
@@ -38,17 +40,17 @@ class CustomModel:
         X_shortcut = X
 
         # First component of the main path
-        X = Conv2D(filters= f1, kernel_size=(f,f), strides=(1,1), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(filters= f1, kernel_size=(1,1), strides=(1,1), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
         X = Activation('relu')(X)
 
         # Second component of the main path
-        X = Conv2D(filters= f2, kernel_size=(f,f), strides=(1,1), padding='valid', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(filters= f2, kernel_size=(f,f), strides=(1,1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
         X = Activation('relu')(X)
 
         # Third component of the main path
-        X = Conv2D(filters= f3, kernel_size=(f,f), strides=(1,1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(filters= f3, kernel_size=(1,1), strides=(1,1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
 
         # Add the shortcut value to the main path
@@ -84,21 +86,21 @@ class CustomModel:
         X_Shortcut = X
 
         # First component of the main path
-        X = Conv2D(f1, (1,1),strides=(s,s),name=conv_name_base + '2a',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(f1, kernel_size=(1,1),strides=(s,s),name=conv_name_base + '2a',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
         X = Activation('relu')(X)
 
         # Second component of the main path
-        X = Conv2D(f2, (1,1),strides=(s,s),name=conv_name_base + '2b',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(f2, kernel_size=(f,f),strides=(1,1),name=conv_name_base + '2b',padding='same',kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
         X = Activation('relu')(X)
 
         # Third component of the main path
-        X = Conv2D(f3, (1,1),strides=(s,s),name=conv_name_base + '2c',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(f3, kernel_size=(1,1),strides=(1,1),name=conv_name_base + '2c',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
 
         # shortcut path
-        X_Shortcut = Conv2D(f3, (1,1),strides=(s,s),name=conv_name_base + '1',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X_Shortcut)
+        X_Shortcut = Conv2D(f3, kernel_size=(1,1),strides=(s,s),name=conv_name_base + '1',padding='valid',kernel_initializer=glorot_uniform(seed=0))(X_Shortcut)
         X_Shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_Shortcut)
 
         # Add main and shortcut path
@@ -107,7 +109,7 @@ class CustomModel:
 
         return X
 
-    def Resnet50(self,input_shape=(32,32,3), classes=20):
+    def Resnet50(self,input_shape=(32,32,3), classes=10):
         """
         Implementation of the popular ResNet50 the following architecture:
         CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
@@ -128,37 +130,37 @@ class CustomModel:
         X = ZeroPadding2D((3,3))(X_input)
 
         # Stage 1
-        X = Conv2D(32, (7,7), strides = (2,2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = Conv2D(64, (7,7), strides = (2,2), name='conv1', kernel_initializer=glorot_uniform(seed=0))(X)
         X = BatchNormalization(axis=3, name='bn_conv1')(X)
         X = Activation('relu')(X)
         X = MaxPooling2D((3,3), strides=(2,2))(X)
-
+    
         # Stage 2
-        X = self.convolutional_block(X, f=3, filters=[32,32,256], stage=2, block='a', s=1)
-        X = self.identity_block(X, 3,[32,32,256], stage=2, block='b')
-        X = self.identity_block(X,3,[32,32,256], stage=2, block='c')
+        X = self.convolutional_block(X, f=3, filters=[64,64,256], stage=2, block='a', s=1)
+        X = self.identity_block(X, 3,[64,64,256], stage=2, block='b')
+        X = self.identity_block(X,3,[64,64,256], stage=2, block='c')
 
         # Stage 3
-        X = self.convolutional_block(X, f=3, filters=[64,64,512], stage=3, block='a', s=1)
-        X = self.identity_block(X,3,[64,64,512], stage=3, block='b')
-        X = self.identity_block(X,3,[64,64,512], stage=3, block='c')
-        X = self.identity_block(X,3,[64,64,512], stage=3, block='d')
+        X = self.convolutional_block(X, f=3, filters=[128,128,512], stage=3, block='a', s=2)
+        X = self.identity_block(X,3,[128,128,512], stage=3, block='b')
+        X = self.identity_block(X,3,[128,128,512], stage=3, block='c')
+        X = self.identity_block(X,3,[128,128,512], stage=3, block='d')
 
         # Stage 4
-        X = self.convolutional_block(X, f=3, filters=[128,128,1024], stage=4, block='a', s=1)
-        X = self.identity_block(X,3,[128,128,1024], stage=4, block='b')
-        X = self.identity_block(X,3,[128,128,1024], stage=4, block='c')
-        X = self.identity_block(X,3,[128,128,1024], stage=4, block='d')
-        X = self.identity_block(X,3,[128,128,1024], stage=4, block='e')
-        X = self.identity_block(X,3,[128,128,1024], stage=4, block='f')
+        X = self.convolutional_block(X, f=3, filters=[256,256,1024], stage=4, block='a', s=2)
+        X = self.identity_block(X,3,[256,256,1024], stage=4, block='b')
+        X = self.identity_block(X,3,[256,256,1024], stage=4, block='c')
+        X = self.identity_block(X,3,[256,256,1024], stage=4, block='d')
+        X = self.identity_block(X,3,[256,256,1024], stage=4, block='e')
+        X = self.identity_block(X,3,[256,256,1024], stage=4, block='f')
 
         # Stage 5
-        X = self.convolutional_block(X, f=3, filters=[256,256,2048], stage=5, block='a', s=1)
-        X = self.identity_block(X,3,[256,256,2048], stage=5, block='b')
-        X = self.identity_block(X,3,[256,256,2048], stage=5, block='c')
+        X = self.convolutional_block(X, f=3, filters=[512,512,2048], stage=5, block='a', s=2)
+        X = self.identity_block(X,3,[512,512,2048], stage=5, block='b')
+        X = self.identity_block(X,3,[512,512,2048], stage=5, block='c')
 
         # AVGPOOL
-        X = AveragePooling2D((2,2))(X)
+        X = AveragePooling2D((1,1))(X)
 
         # Output Layer
         X = Flatten()(X)
@@ -170,47 +172,29 @@ class CustomModel:
 
     def train_with_custom_resnet50(self, X_train, Y_train, X_test, Y_test, num_epochs, batch_size):
         model = self.Resnet50()
+        print(model.summary())
+        util = Utility()
         
         # Compile the model
-        model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer=Adam(lr=util.lr_schedule(0), epsilon=1e-8),loss='categorical_crossentropy', metrics=['accuracy'])
+
+        modelCheckpoint = ModelCheckpoint(filepath=util.getModelPath(),monitor='val_acc',verbose=1, save_best_only=True)
+        learningRateScheduler = LearningRateScheduler(util.lr_schedule)
+        lrReducer = ReduceLROnPlateau(factor=np.sqrt(0.1), cooldown=0.0, patience=5, min_lr=0.5e-6)
+        #earlyStop = EarlyStopping(monitor='val_loss', patience=5, min_delta=1e-3,restore_best_weights=True,mode='auto')
+
+        # Prepare callbacks
+        callbacks = [modelCheckpoint, learningRateScheduler, lrReducer]
         
         # Train the model
-        model.fit(X_train,Y_train,epochs=num_epochs,batch_size=batch_size)
+        model.fit(X_train,Y_train,epochs=num_epochs,batch_size=batch_size,validation_data=(X_test, Y_test), shuffle=True, callbacks=callbacks)
 
         # Evaluate the model
-        _,test_loss = model.evaluate(X_test, Y_test)
-        print("Accuracy on the test set: " + str(test_loss * 100) + "%")
+        _,test_acc = model.evaluate(X_test, Y_test)
+        print("Accuracy on the test set: " + str(test_acc * 100) + "%")
 
         return model
-
-    def transfer_learn_resnet50(self, X_train, Y_train, X_test, Y_test, num_epochs, batch_size):
-        # Load the model 
-        resnet = ResNet50(include_top=True, weights='imagenet')
-       
-        X = resnet.output
-        #X = GlobalAveragePooling2D()(X)
-        X = Dense(512, activation='relu', name='fc-1')(X)
-        X = Dropout(0.5)(X)
-        X = Dense(256, activation='relu', name='fc-2')(X)
-        X = Dropout(0.5)(X)
-        out = Dense(20, activation='softmax', name='output')(X)
-
-        model = keras.models.Model(inputs=resnet.input, outputs=out)
-        print(model.summary())
-
-        # Switch off first layer
-        for layer in model.layers[:-6]:
-            layer.trainable = False
-
-        # Switch off trainable for layers not to be trained
-        model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-        model.fit(X_train, Y_train, epochs=num_epochs, batch_size=batch_size)
-
-        _, test_accuracy = X.evaluate(X_test, Y_test)
-        print("Test Accuracy: " + str(test_accuracy))
-
-        return model
-
+    
     def predict_with_resnet50(self, image_path, model):
         # Preprocess the image
         img = image.load_img(image_path, target_size=(224, 224))
